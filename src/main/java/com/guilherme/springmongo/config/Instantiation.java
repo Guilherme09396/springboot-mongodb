5298e514de138696230cf014aa71fff09d19eb6e
Guilherme Gomes <guilherme09396@gmail.com>
2023-07-25 10:32:01 -0300

    Projeção de dados com Author dto

--- src/main/java/com/guilherme/springmongo/config/Instantiation.java
+++ src/main/java/com/guilherme/springmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.guilherme.springmongo.domain.Post;
 import com.guilherme.springmongo.domain.User;
+import com.guilherme.springmongo.dto.AuthorDTO;
 import com.guilherme.springmongo.repository.PostRepository;
 import com.guilherme.springmongo.repository.UserRepository;
 
@@ -35,8 +36,8 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, abraços!", maria );
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, abraços!", new AuthorDTO(maria) );
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
