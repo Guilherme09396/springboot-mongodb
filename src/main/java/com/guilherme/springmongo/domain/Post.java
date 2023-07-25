360789b66b06451c19da7f9d37711f6b41e47f68
Guilherme Gomes <guilherme09396@gmail.com>
2023-07-25 11:22:07 -0300

    Referenciando os posts do usuário

--- src/main/java/com/guilherme/springmongo/domain/Post.java
+++ src/main/java/com/guilherme/springmongo/domain/Post.java
@@ -8,7 +8,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.guilherme.springmongo.dto.AuthorDTO;
-import com.guilherme.springmongo.dto.UserDTO;
 
 @Document
 public class Post implements Serializable {
