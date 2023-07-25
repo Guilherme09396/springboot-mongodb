bd078aa7064b96b6bcaaf2571c9984306eae804b
Guilherme Gomes <guilherme09396@gmail.com>
2023-07-25 11:27:19 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/guilherme/springmongo/resources/UserResource.java
+++ src/main/java/com/guilherme/springmongo/resources/UserResource.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.guilherme.springmongo.domain.Post;
 import com.guilherme.springmongo.domain.User;
 import com.guilherme.springmongo.dto.UserDTO;
 import com.guilherme.springmongo.services.UserService;
@@ -59,5 +60,11 @@ public class UserResource {
 		user.setId(id);
 		service.update(user);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok(user.getPosts());
 	}
 }
