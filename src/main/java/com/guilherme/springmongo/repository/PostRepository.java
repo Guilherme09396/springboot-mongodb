ccd369839d54d0a637f44334f4c03869ab67cb4b
Guilherme Gomes <guilherme09396@gmail.com>
2023-07-25 12:33:55 -0300

    Consulta simples com @Query

--- src/main/java/com/guilherme/springmongo/repository/PostRepository.java
+++ src/main/java/com/guilherme/springmongo/repository/PostRepository.java
@@ -3,9 +3,13 @@ package com.guilherme.springmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.guilherme.springmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 	List<Post> findByTitleContaining(String text);
+	
+	@Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
 }
