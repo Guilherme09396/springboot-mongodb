558790462de95b1b847b774a150046157e30eee7
Guilherme Gomes <guilherme09396@gmail.com>
2023-07-25 12:23:36 -0300

    Consulta simples com query methods

--- src/main/java/com/guilherme/springmongo/repository/PostRepository.java
+++ src/main/java/com/guilherme/springmongo/repository/PostRepository.java
@@ -1,9 +1,11 @@
 package com.guilherme.springmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.guilherme.springmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	List<Post> findByTitleContaining(String text);
 }
---
+++ src/main/java/com/guilherme/springmongo/resources/util/URL.java
@@ -0,0 +1,14 @@
+package com.guilherme.springmongo.resources.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class URL {
+	public static String decodeUrl(String text) {
+		try {
+			return URLDecoder.decode(text, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+}
