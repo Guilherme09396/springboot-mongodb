5406c23069bfc983cc7a10bcac8f276fbb0ebd76
Guilherme Gomes <guilherme09396@gmail.com>
2023-07-25 11:33:35 -0300

    Obtendo post por Id

---
+++ src/main/java/com/guilherme/springmongo/services/PostService.java
@@ -0,0 +1,20 @@
+package com.guilherme.springmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilherme.springmongo.domain.Post;
+import com.guilherme.springmongo.repository.PostRepository;
+import com.guilherme.springmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Post post = repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		return post;
+	}
+}
