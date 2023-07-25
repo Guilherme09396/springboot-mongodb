a8400659bddb359f59846a4f94299a0fa4e8225b
Guilherme Gomes <guilherme09396@gmail.com>
2023-07-25 11:47:35 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/guilherme/springmongo/config/Instantiation.java
+++ src/main/java/com/guilherme/springmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.guilherme.springmongo.domain.Post;
 import com.guilherme.springmongo.domain.User;
 import com.guilherme.springmongo.dto.AuthorDTO;
+import com.guilherme.springmongo.dto.CommentDTO;
 import com.guilherme.springmongo.repository.PostRepository;
 import com.guilherme.springmongo.repository.UserRepository;
 
@@ -42,6 +43,14 @@ public class Instantiation implements CommandLineRunner {
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2021"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2021"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
+		postRepository.save(post1);
 	}
 
 }
